d67dadb24c4955255a41af153923c63a889619a4
drPilman <doktorpilman@gmail.com>
2024-12-01 13:38:29 +0300

    db fix

--- backend/src/services/chroma.py
+++ backend/src/services/chroma.py
@@ -1,8 +1,9 @@
 import chromadb
 from settings import settings
+from typing import List
 
 
-def delete_from_chroma(cls: int):
+def delete_from_chroma(cls: int) -> None:
     chroma_client = chromadb.HttpClient(host=settings.CHROMADB_URL, port=settings.CHROMADB_PORT)
     collection = chroma_client.get_or_create_collection(name=settings.CHROMADB_NAME)
     result = collection.fetch(where={"class": cls})
@@ -11,3 +12,14 @@ def delete_from_chroma(cls: int):
 
     if ids_to_delete:
         collection.delete(ids=ids_to_delete)
+
+
+def insert_to_chroma(cls: int,
+                     input_data: List[float]) -> None:
+    chroma_client = chromadb.HttpClient(host=settings.CHROMADB_URL, port=settings.CHROMADB_PORT)
+    collection = chroma_client.get_or_create_collection(name=settings.CHROMADB_NAME)
+    collection.add(
+        ids=[cls],
+        embeddings=[input_data],
+        metadatas=["class"]
+    )
